cbde0a0212ca91a1a021aa08b28a80b4cb559b8d
harman-dhami <86544524+harman-dhami@users.noreply.github.com>
2022-11-30 17:09:07 -0800

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "family_mart_admin";
+$dbPassword = "pa55word";
+$dbName = "family_mart";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
